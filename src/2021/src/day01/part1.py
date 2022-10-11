1eae75607d4ecf8e96fbf706c518ef76033460ce
Nicholas Ragland <nragland37@gmail.com>
2022-10-10 23:53:22 -0500

    updated solution

--- src/2021/src/day01/part1.py
+++ src/2021/src/day01/part1.py
@@ -1,40 +1,43 @@
-# numbers = [
-#     199,
-#     200,
-#     208,
-#     210,
-#     200,
-#     207,
-#     240,
-#     269,
-#     260,
-#     263,
-# ]
+INPUT = """\
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+"""
 
-file = open("input01.txt")
-lines = file.readlines()
-numbers = []
+def main():
+    numbers = open("input01.txt", "r").read().splitlines()       # file input
+    #numbers = [int(line.strip()) for line in INPUT.splitlines()]  # example input
+    print(numbers[0], "(N/A - no previous measurement)")
 
-for line in lines:
-    number = int(line.strip())
-    numbers.append(number)
+    count = 0
 
-print(numbers[0], "(N/A - no previous measurement)")
+    for index in range(1, len(numbers)):
+        currentNum = numbers[index]
+        prevNum = numbers[index - 1]
 
-count = 0
+        if currentNum > prevNum:
+            print(currentNum, "(increased)")
+            count += 1
+        else:
+            print(currentNum, "(decreased)")
 
-for index in range(1, len(numbers)):
+    print("")
+    print("Solution:", count)
 
-    currentNum = numbers[index]
-    prevNum = numbers[index - 1]
+if __name__ == "__main__":
+    main()
 
-    if currentNum > prevNum:
-        print(currentNum, "(increased)")
-        count += 1
-    else:
-        print(currentNum, "(decreased)")
 
-print("")
-print("Solution:", count)
+# example output:
+# Solution: 7
 
-#Solution: 1226
+
+# file output:
+# Solution: 1225
