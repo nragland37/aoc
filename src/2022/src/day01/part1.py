24b3d5c6505379cd7ca50c28864ec9b0e2345653
Nicholas Ragland <nragland37@gmail.com>
2023-12-09 03:01:37 -0600

    cleaned 2022 day 1

--- src/2022/src/day01/part1.py
+++ src/2022/src/day01/part1.py
@@ -1,13 +1,9 @@
 def find_max_calorie_elf(calories_list):
-    # Initialize the dictionary to store the total Calories for each Elf
     calories = {}
     current_elf = "Elf 1"
     calories[current_elf] = 0
-
-    # Initialize a counter to keep track of the current Elf
     counter = 1
 
-    # Iterate through the list of Calories and update the total Calories for each Elf
     for calorie in calories_list:
         if calorie == "":
             counter += 1
@@ -18,24 +14,20 @@ def find_max_calorie_elf(calories_list):
         else:
             print("Invalid input: " + calorie)
 
-    # Find the Elf with the most Calories and print the Elf's name and total Calories
     max_calories = max(calories.values())
     max_elf = [key for key in calories if calories[key] == max_calories]
-    print("The Elf carrying the most Calories is " +
-          max_elf[0] + " with a total of " + str(max_calories) + " Calories.")
+    print(f"{max_calories}\n{max_elf}")
+
+
+# **************************************************************************************************
 
 
 def main():
-# Read the input from the file and split the lines into a list of strings
     calories_list = open("input01.txt").read().splitlines()
-
-    # Find the Elf carrying the most Calories
     find_max_calorie_elf(calories_list)
 
 
+# **************************************************************************************************
+
 if __name__ == "__main__":
     main()
-
-#solution: 70698
-
-#The Elf carrying the most Calories is Elf 65 with a total of 70698 Calories.
--- src/2022/src/day01/part2.py
+++ src/2022/src/day01/part2.py
@@ -1,13 +1,9 @@
 def find_max_calorie_elf(calories_list):
-    # Initialize the dictionary to store the total Calories for each Elf
     calories = {}
     current_elf = "Elf 1"
     calories[current_elf] = 0
-
-    # Initialize a counter to keep track of the current Elf
     counter = 1
 
-    # Iterate through the list of Calories and update the total Calories for each Elf
     for calorie in calories_list:
         if calorie == "":
             counter += 1
@@ -18,30 +14,25 @@ def find_max_calorie_elf(calories_list):
         else:
             print("Invalid input: " + calorie)
 
-    # Sort the Elves by total Calories in descending order
-    sorted_calories = sorted(
-        calories.items(), key=lambda x: x[1], reverse=True)
+    sorted_calories = sorted(calories.items(), key=lambda x: x[1], reverse=True)
 
-    # Return the top three Elves
     return sorted_calories[:3]
 
 
+# **************************************************************************************************
+
+
 def main():
-    # Read the input from the file and split the lines into a list of strings
     calories_list = open("input01.txt").read().splitlines()
 
-    # Find the top three Elves carrying the most Calories
     top_elves = find_max_calorie_elf(calories_list)
 
-    # Calculate the total Calories carried by the top three Elves
     total_calories = sum(elf[1] for elf in top_elves)
-    print("The top three Elves are carrying a total of " +
-          str(total_calories) + " Calories.")
+    print(total_calories)
+
+
+# **************************************************************************************************
 
 
 if __name__ == "__main__":
     main()
-
-# solution: 206643
-
-# The top three Elves are carrying a total of 206643 Calories.
